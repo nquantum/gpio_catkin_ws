77d4329dcd5b23048d5494f7e8e5791f12b8ef3a
Prapan <nquantum@hotmail.com>
2020-01-09 11:07:11 +0700

    tidy talker before test spin/call back

--- talker_pynput.py
+++ talker_pynput.py
@@ -8,18 +8,6 @@ from pynput import keyboard
 #    rospy.loginfo("press key is: " + key.char)
 #    pub.publish("pressed")
     
-    #if key.char == 'a':
-    #    rospy.loginfo('RED')
-    #    pub.publish('red')
-    #elif key.char == 's':
-    #    rospy.loginfo('YELLOW')
-    #    pub.publish('yellow')
-    #elif key.char == 'd':
-    #    rospy.loginfo('GREEN')
-    #    pub.publish('green')
-    #else:
-    #    def on_release(key):
-
 #def on_release(key):
 #    print("release key is: " + key.char)
 #    rospy.loginfo("release key is: " + key.char)
@@ -27,24 +15,24 @@ from pynput import keyboard
 
 def talker():
     def on_press(key):
-        rospy.loginfo("press key is: " + key.char)        
+        rospy.loginfo("press key is: " + key.char)
+        #pub.publish("pressed: " + key.char)
         if key.char == 'a':
             pub.publish('red')
         elif key.char == 's':
             pub.publish('yellow')
         elif key.char == 'd':
             pub.publish('green')        
-        #pub.publish("pressed: " + key.char)
 
     def on_release(key):
         rospy.loginfo("release key is: " + key.char)
+        #pub.publish("released: " + key.char)
         if key.char == 'a':
             pub.publish('no-red')
         elif key.char == 's':
             pub.publish('no-yellow')
         elif key.char == 'd':
             pub.publish('no-green')
-        #pub.publish("released: " + key.char)
 
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('driver', anonymous=True)
@@ -52,7 +40,7 @@ def talker():
     
     # begin scankey program
     rospy.loginfo('Control LED by ASD key.')
-#    pub.publish("started")
+    pub.publish("start")
 
     # non-blocking
     listener = keyboard.Listener(
@@ -60,6 +48,7 @@ def talker():
         on_release=on_release)
     listener.start()
 
+    #rospy.spin()
     while not rospy.is_shutdown():
         rate.sleep()
 
